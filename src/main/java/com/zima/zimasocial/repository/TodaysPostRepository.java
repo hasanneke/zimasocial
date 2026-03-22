422f3273959d57c856a236bc0f238990fd1c7e20
Hasan Eke <128808077+hasanneke@users.noreply.github.com>
2026-03-22 23:56:27 +0300

    fix read todays post

--- src/main/java/com/zima/zimasocial/repository/TodaysPostRepository.java
+++ src/main/java/com/zima/zimasocial/repository/TodaysPostRepository.java
@@ -2,10 +2,19 @@ package com.zima.zimasocial.repository;
 
 import com.zima.zimasocial.entity.todayspost.TodaysPost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface TodaysPostRepository extends JpaRepository<TodaysPost, Long> {
+    @Query("""
+        SELECT todaysPost FROM TodaysPost todaysPost
+        WHERE todaysPost.author.isBanned = false
+        AND todaysPost.author.isDisabled = false
+        AND todaysPost.author.isPrivate = false 
+        AND todaysPost.author.isDeleted = false
+        AND todaysPost.date = :date
+        """)
     List<TodaysPost> findTodaysPostByDate(LocalDate date);
 }
