0c23a68d617b57defe555cefe30977be16acbb49
Hasan Eke <128808077+hasanneke@users.noreply.github.com>
2026-04-04 04:30:46 +0300

    disable event listener

--- src/main/java/com/zima/zimasocial/context/social/playlist/listener/PlaylistEventListener.java
+++ src/main/java/com/zima/zimasocial/context/social/playlist/listener/PlaylistEventListener.java
@@ -4,7 +4,6 @@ import com.zima.zimasocial.context.account.event.AccountCreatedEvent;
 import com.zima.zimasocial.context.social.author.value.AuthorId;
 import com.zima.zimasocial.context.social.playlist.application.PlayListService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,7 +11,7 @@ import org.springframework.stereotype.Component;
 public class PlaylistEventListener {
     private final PlayListService playListService;
 
-    @EventListener
+//    @EventListener
     public void handleNewAccountCreated(AccountCreatedEvent accountCreatedEvent) {
         playListService.createDefaultPlaylistsForAuthor(new AuthorId(accountCreatedEvent.accountId()));
     }
