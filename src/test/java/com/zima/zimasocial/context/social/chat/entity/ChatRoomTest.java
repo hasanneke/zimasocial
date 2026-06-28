0e9d74598b7b45192102943f97699ee07f8bfec6
Hasan Eke <128808077+hasanneke@users.noreply.github.com>
2026-06-28 21:13:34 +0300

    fix tests, update pom version

--- src/test/java/com/zima/zimasocial/context/social/chat/entity/ChatRoomTest.java
+++ src/test/java/com/zima/zimasocial/context/social/chat/entity/ChatRoomTest.java
@@ -1,9 +1,11 @@
 package com.zima.zimasocial.context.social.chat.entity;
 
+import com.github.f4b6a3.uuid.UuidCreator;
 import com.zima.zimasocial.AuthorFixture;
 import com.zima.zimasocial.context.social.author.entity.Author;
 import com.zima.zimasocial.context.social.chat.ChatTestUtility;
 import com.zima.zimasocial.context.social.chat.exception.AuthorIsNotInRoom;
+import com.zima.zimasocial.context.social.chat.value.ChatMessageId;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,7 +20,7 @@ public class ChatRoomTest {
         Author sender = AuthorFixture.validAuthor();
         Author receiver = AuthorFixture.validAuthor();
         Assertions.assertThrows(AuthorIsNotInRoom.class,
-                () -> chatRoom.sendMessage("", sender));
+                () -> chatRoom.sendMessage(new ChatMessageId(UuidCreator.getTimeOrdered()),"", sender));
     }
 
     @Test
@@ -28,7 +30,7 @@ public class ChatRoomTest {
 
         ChatRoom chatRoom = ChatTestUtility.mockChatRoom(sender.getId().getValue(), 1L);
         // Sender is not in room
-        ChatMessage chatMessage = chatRoom.sendMessage("Message", sender);
+        ChatMessage chatMessage = chatRoom.sendMessage(new ChatMessageId(UuidCreator.getTimeOrdered()),"Message", sender);
         Assertions.assertNotNull(chatMessage.getContent());
         Assertions.assertEquals(chatMessage.getSenderId(), sender.getId());
     }
